35362f2904365ea3f3d30888530ee6d4f77d2e55
Oleg <oleg.pav.m@gmail.com>
2019-01-23 18:51:31 +0300

    #2 Locales draft.

---
+++ src/data/locale.js
@@ -0,0 +1,20 @@
+const core = require('gls-core-service');
+const Template = core.utils.Template;
+
+// prettier-ignore
+module.exports = Template.makeFor({
+    registration: {
+        hello: {
+            ru: 'ПРИВЕТСТВИЕ НА СТАРТЕ',
+            en: 'ЕН ПРИВЕТСТВИЕ НА СТАРТЕ',
+        },
+        resendCode: {
+            ru: 'ОПОВЕЩЕНИЕ ЧТО КОД БУДЕТ ИЗМЕНЕН',
+            en: 'ЕН ОПОВЕЩЕНИЕ ЧТО КОД БУДЕТ ИЗМЕНЕН',
+        },
+        code: {
+            ru: 'Ваш код подтверждения ${code}',
+            en: 'Your verification code is ${code}',
+        },
+    },
+});
